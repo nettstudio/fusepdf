472f6b2f8a31ec575952cb71f45146bbfde9c8ec
Ole-André Rodlie <ole.andre.rodlie@gmail.com>
2021-01-21 22:06:19 +0100

    initial functions

--- fusepdf.cpp
+++ fusepdf.cpp
@@ -4,12 +4,128 @@
 FusePDF::FusePDF(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::FusePDF)
+    , _proc(Q_NULLPTR)
 {
     ui->setupUi(this);
+    _proc = new QProcess(this);
+    connect(_proc, SIGNAL(finished(int)),
+            this, SLOT(commandFinished(int)));
+    connect(_proc, SIGNAL(started()),
+            this, SLOT(commandStarted()));
+    connect(ui->inputs, SIGNAL(addedItem()),
+            this, SLOT(handleAddedItem()));
+    loadSettings();
 }
 
 FusePDF::~FusePDF()
 {
+    saveSettings();
     delete ui;
 }
 
+
+void FusePDF::on_actionOpen_triggered()
+{
+    qDebug() << "on_actionOpen_triggered";
+}
+
+void FusePDF::on_actionSave_triggered()
+{
+    qDebug() << "on_actionSave_triggered";
+}
+
+void FusePDF::on_actionClear_triggered()
+{
+    qDebug() << "on_actionClear_triggered";
+}
+
+void FusePDF::on_actionQuit_triggered()
+{
+    qDebug() << "on_actionQuit_triggered";
+}
+
+void FusePDF::on_actionAbout_triggered()
+{
+    qDebug() << "on_actionAbout_triggered";
+}
+
+void FusePDF::on_paper_currentIndexChanged(const QString &arg1)
+{
+    qDebug() << "on_paper_currentIndexChanged" << arg1;
+}
+
+void FusePDF::on_preset_currentIndexChanged(const QString &arg1)
+{
+    qDebug() << "on_preset_currentIndexChanged" << arg1;
+}
+
+void FusePDF::on_compat_currentIndexChanged(const QString &arg1)
+{
+    qDebug() << "on_compat_currentIndexChanged" << arg1;
+}
+
+void FusePDF::on_dpi_valueChanged(int arg1)
+{
+    qDebug() << "on_dpi_valueChanged" << arg1;
+}
+
+void FusePDF::on_fileButton_clicked()
+{
+    qDebug() << "on_fileButton_clicked";
+}
+
+void FusePDF::on_clear_clicked()
+{
+    qDebug() << "on_clear_clicked";
+}
+
+void FusePDF::makeCommand()
+{
+    qDebug() << "makeCommand";
+}
+
+void FusePDF::runCommand()
+{
+    qDebug() << "runCommand";
+}
+
+void FusePDF::commandStarted()
+{
+    qDebug() << "commandStarted";
+}
+
+void FusePDF::commandFinished(int exitCode)
+{
+    Q_UNUSED(exitCode)
+    qDebug() << "commandfinished" << exitCode;
+}
+
+void FusePDF::populateUI()
+{
+    qDebug() << "populateUI";
+}
+
+void FusePDF::loadSettings()
+{
+    qDebug() << "loadSettings";
+}
+
+void FusePDF::saveSettings()
+{
+    qDebug() << "saveSettings";
+}
+
+void FusePDF::on_dpiCheck_stateChanged(int arg1)
+{
+    qDebug() << "on_dpiCheck_stateChanged" << arg1;
+}
+
+void FusePDF::on_save_clicked()
+{
+    qDebug() << "on_save_clicked";
+}
+
+void FusePDF::handleAddedItem()
+{
+    qDebug() << "handleAddedItem";
+}
--- fusepdf.h
+++ fusepdf.h
@@ -3,6 +3,71 @@
 
 #include <QMainWindow>
 
+#include <QDropEvent>
+#include <QTreeWidget>
+#include <QMimeData>
+#include <QMimeDatabase>
+#include <QFileInfo>
+#include <QDebug>
+#include <QProcess>
+#include <QProcessEnvironment>
+
+class FilesTreeWidget : public QTreeWidget
+{
+    Q_OBJECT
+public:
+    FilesTreeWidget(QWidget *parent= Q_NULLPTR):
+        QTreeWidget(parent)
+    {
+        setAcceptDrops(true);
+        setDropIndicatorShown(true);
+        setColumnCount(1);
+    }
+
+signals:
+    void addedItem();
+
+protected:
+    bool dropMimeData(QTreeWidgetItem *parent, int /*index*/, const QMimeData *data, Qt::DropAction /*action*/)
+    {
+        qDebug() << data->urls();
+        for(const QUrl url: data->urls()) {
+            const QFileInfo info( url.toLocalFile());
+            if (info.isFile() ){
+                QMimeDatabase db;
+                QMimeType type = db.mimeTypeForFile(info.absoluteFilePath());
+                if (type.name() != "application/pdf") {
+                    qDebug() << "unsupported format" << info.fileName() << type.name();
+                    continue;
+                }
+                QTreeWidgetItem *item;
+                if (parent){
+                    item = new QTreeWidgetItem(parent);
+                    parent->setExpanded(true);
+                }
+                else {
+                    item = new QTreeWidgetItem(this);
+                }
+                item->setText(0, info.fileName());
+                item->setText(1, info.filePath());
+                emit addedItem();
+            }
+        }
+        return true;
+    }
+
+    QStringList mimeTypes () const
+    {
+        return QStringList()<<"text/uri-list";
+    }
+
+    Qt::DropActions supportedDropActions () const
+    {
+        return Qt::CopyAction;
+    }
+
+};
+
 QT_BEGIN_NAMESPACE
 namespace Ui { class FusePDF; }
 QT_END_NAMESPACE
@@ -15,7 +80,32 @@ public:
     FusePDF(QWidget *parent = nullptr);
     ~FusePDF();
 
+private slots:
+    void on_actionOpen_triggered();
+    void on_actionSave_triggered();
+    void on_actionClear_triggered();
+    void on_actionQuit_triggered();
+    void on_actionAbout_triggered();
+    void on_paper_currentIndexChanged(const QString &arg1);
+    void on_preset_currentIndexChanged(const QString &arg1);
+    void on_compat_currentIndexChanged(const QString &arg1);
+    void on_dpi_valueChanged(int arg1);
+    void on_fileButton_clicked();
+    void on_clear_clicked();
+    void makeCommand();
+    void runCommand();
+    void commandStarted();
+    void commandFinished(int exitCode);
+    void populateUI();
+    void loadSettings();
+    void saveSettings();
+    void on_dpiCheck_stateChanged(int arg1);
+    void on_save_clicked();
+    void handleAddedItem();
+
 private:
     Ui::FusePDF *ui;
+    QString _cmd;
+    QProcess *_proc;
 };
 #endif // FUSEPDF_H
