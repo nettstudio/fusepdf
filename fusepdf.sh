e277590093049acdaafcfbd4dca4f28eecf85f30
Ole-André Rodlie <ole.andre.rodlie@gmail.com>
2021-01-23 18:42:54 +0100

    deploy stuff

--- fusepdf.sh
+++ fusepdf.sh
@@ -5,11 +5,18 @@ BUILD_DIR=${BUILD_DIR:-"${CWD}/tmp"}
 MXE=${MXE:-"${CWD}/mxe"}
 MXE_TC=${MXE_TC:-x86_64-w64-mingw32.static}
 BIT=${BIT:-64}
+SNAPSHOT=${SNAPSHOT:-1}
 STRIP=${MXE_TC}-strip
 TIMESTAMP=${TIMESTAMP:-`date +%Y%m%d%H%M`}
 VERSION=`cat $CWD/fusepdf.pro | sed '/VERSION =/!d' | awk '{print $3}'`
 COMMIT=`git rev-parse --short HEAD`
-ZIP="FusePDF-$VERSION-$TIMESTAMP-$COMMIT-Windows-x$BIT"
+ZIP="FusePDF-$VERSION-$TIMESTAMP-$COMMIT-Windows-x${BIT}.zip"
+FOLDER="FusePDF-$VERSION-$TIMESTAMP-$COMMIT"
+
+if [ "$SNAPSHOT" = 0 ]; then
+    ZIP="FusePDF-$VERSION-Windows-x$BIT.zip"
+    FOLDER="FusePDF-$VERSION"
+fi
 
 if [ ! -d "${MXE}" ]; then
     echo "Please setup MXE!"
@@ -23,12 +30,12 @@ export PKG_CONFIG_PATH="${MXE}/usr/${MXE_TC}/lib/pkgconfig"
 
 qmake CONFIG+=release ${CWD} || exit 1
 make || exit 1
-mkdir $ZIP $ZIP/platforms $ZIP/opensource || exit 1
-cp -a $CWD/legal/* $ZIP/opensource/ || exit 1
-cp $MXE/usr/$MXE_TC/qt5/plugins/platforms/qwindows.dll $ZIP/platforms/ || exit 1
-cp $MXE/usr/$MXE_TC/qt5/bin/{Qt5CoreNettStudio.dll,Qt5GuiNettStudio.dll,Qt5WidgetsNettStudio.dll} $ZIP/ || exit 1
-cp release/FusePDF.exe $ZIP || exit 1
-$STRIP -s $ZIP/*.exe $ZIP/*.dll $ZIP/*/*.dll || exit 1
-zip -r -9 $ZIP.zip $ZIP || exit 1
-mv $ZIP.zip .. || exit 1
+mkdir $FOLDER $FOLDER/platforms $FOLDER/opensource || exit 1
+cp -a $CWD/legal/* $FOLDER/opensource/ || exit 1
+cp $MXE/usr/$MXE_TC/qt5/plugins/platforms/qwindows.dll $FOLDER/platforms/ || exit 1
+cp $MXE/usr/$MXE_TC/qt5/bin/{Qt5CoreNettStudio.dll,Qt5GuiNettStudio.dll,Qt5WidgetsNettStudio.dll} $FOLDER/ || exit 1
+cp release/FusePDF.exe $FOLDER || exit 1
+$STRIP -s $FOLDER/*.exe $FOLDER/*.dll $FOLDER/*/*.dll || exit 1
+zip -r -9 $ZIP $FOLDER || exit 1
+mv $ZIP .. || exit 1
 echo "DONE!"
