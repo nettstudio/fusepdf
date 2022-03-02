8cbcbb1bf761a3f065b1aa8aa0f453e62e95bde2
Ole-André Rodlie <ole.andre.rodlie@gmail.com>
2022-03-02 18:47:32 +0100

    minor

--- tools/mkdoc.sh
+++ tools/mkdoc.sh
@@ -4,7 +4,6 @@
 #
 
 #
-# apt-get install autoconf automake autopoint bash bison bzip2 flex g++ g++-multilib gettext git gperf intltool libc6-dev-i386 libgdk-pixbuf2.0-dev libltdl-dev libssl-dev libtool-bin libxml-parser-perl lzip make openssl p7zip-full patch perl python ruby sed unzip wget xz-utils
 # sudo apt-get install texlive-xetex
 # https://github.com/jgm/pandoc/releases/download/2.7.2/pandoc-2.7.2-linux.tar.gz
 #
