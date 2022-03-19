8ef43e7eecb7b13ce987a961920db184295b93d4
Ole-André <ole.andre.rodlie@gmail.com>
2022-03-20 00:56:27 +0100

    Update fusepdf.bat

--- fusepdf.bat
+++ fusepdf.bat
@@ -3,5 +3,9 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64 %*
 set PATH=C:\qt-5.12\bin;%PATH%
 
-qmake.exe CONFIG+=release
+@RD /S /Q build
+mkdir build
+cd build
+
+qmake.exe CONFIG+=release ..
 nmake
